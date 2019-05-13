d89b9990c533c073d14271e8f481a81631b99442
Kevin Anantha <kevin.anantha@gmail.com>
2019-05-14 00:51:48 +0800

    :fire:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_165032) do
+ActiveRecord::Schema.define(version: 2019_05_13_160242) do
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_165032) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_shot"
   end
 
   create_table "users", force: :cascade do |t|
